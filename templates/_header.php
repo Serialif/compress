e2b24ee5c9ff7251b4b47e49da7f0a411b56d24e
Serialif <contact@serialif.com>
2021-06-02 11:48:06 +0200

    optimize sass and add favicon

--- templates/_header.php
+++ templates/_header.php
@@ -3,6 +3,8 @@
 
     <head>
         <title>Compression d'images</title>
+        <link rel="icon" type="image/png" href="/favicon.png">
+        <link rel="shortcut icon" href="/favicon.png">
         <link rel="stylesheet" href="app.css">
     </head>
 
